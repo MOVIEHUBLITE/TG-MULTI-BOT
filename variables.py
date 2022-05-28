b7d2ba1b5ad37e8e6ff7b6e30f7bb03ee5e1f48f
MoVIe HuB TeAm <92249328+MOVIEHUBLITE@users.noreply.github.com>
2022-05-28 22:46:09 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -8,8 +8,8 @@ STAT_STICK = os.environ.get("STAT_STICK", "CAACAgIAAxkBAAIDwmKSV0bNuMDglK8nzEsWx
 
 PICS = os.environ.get("PICS", "https://telegra.ph/file/448878f9a10e0143a157d.jpg").split()
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMIN', '900873119').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMIN', '1467649219').split()]
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("mongodb+srv://Huzain:Huzain@cluster0.jfppkde.mongodb.net/?retryWrites=true&w=majority", "")
 
 DELAY = int(os.environ.get("DELAY", "1"))
